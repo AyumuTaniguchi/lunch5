28257865f4f39afd7c3ca29b261708050c1c041f
Ayumu Taniguchi <ayumu19921203@gmail.com>
2016-03-28 22:53:19 +0900

    modify: root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'stores/index'
 
-  get 'stores/show'
+
+  root "stores#index"
+  resources :stores, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
